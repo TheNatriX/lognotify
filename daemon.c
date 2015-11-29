847299dd4fd4254d1d45c7208291983660508209
Cristian Ionita <cristian.ionita85@gmail.com>
2015-11-29 03:34:41 +0200

    offset replaced with size and other minor changes

--- daemon.c
+++ daemon.c
@@ -1,6 +1,7 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
 
@@ -9,7 +10,7 @@
 struct  logfile
 {
         int     wd;             /*      watch descriptor        */
-        off_t   offset;         /*      last offset address     */
+        size_t	size;           /*      last known size		*/
         char    name[256];      /*      name of log file        */
 
 } *j;
@@ -20,17 +21,20 @@ int diff_daemon( void )
 {
 	struct stat ss;
 	int fd;
-	int c;
+	int c, x;
 	ssize_t recv_len;
-	char buff[1024];
-	char *super_buff[10];
+	char *buf;
+	char *super_buff[1024];
+
+
+prepare_environment();
 
 	for(;;) {
 
 		j = wait_for_changes();
 		if( j )	{
 
-			/* get current size */
+			/* get the current size */
 			if( stat( j->name, &ss ) == -1 ) {
 				fprintf( stderr, "Cannot stat file \"%s", j->name );
 				perror( "\"" );
@@ -39,31 +43,30 @@ int diff_daemon( void )
 
 			/* if the size is not the same, read content from file */
 			/* TODO: handle conditions when logs were truncated */
-			if( j->offset == ss.st_size )
+			if( j->size == ss.st_size )
 				continue;
+		
+			buf = malloc( ss.st_size - j->size + 1 );
 
 			fd = open( j->name, O_RDONLY );
-			lseek( fd, j->offset, SEEK_SET );
+			lseek( fd, j->size, SEEK_SET );
 
-			while( ( recv_len = read( fd, buff,
-				sizeof( buff ) ) ) > 1 ) {
-
-				write( 1, buff, recv_len );
- 
+			recv_len = read( fd, buf, ss.st_size - j->size );
+		       
+			if( recv_len > 0 ) {
+				*(buf + recv_len) = 0;
+				//puts( buf );
+				draw_on_screen( buf );
 			}
+
 			close( fd );
 
-//			printf( "FILE: %s;\tLAST SIZE: %u;\tCURRENT SIZE: %u\n",
-//				j->name, j->offset, ss.st_size );
-
-
-
 			/* save the current size */
-			j->offset = ss.st_size;
+			j->size = ss.st_size;
+			free(buf);
 		}
 	}
 	
 	return 0;
 }
-
 
