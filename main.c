2932b678aa7b34612aaae5ba57f17b97e1f79105
Cristian Ionita <cristian.ionita85@gmail.com>
2015-12-03 01:47:29 +0200

    accept logfiles from command line

--- main.c
+++ main.c
@@ -1,25 +1,39 @@
 #include <stdio.h>
 #include <libgen.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #include "lognotify.h"
 
-const char *files[] = { "/home/natrix/test", "/home/natrix/test2",
-		"/var/log/syslog", 0 };
+char *files[256];
 
-void print_help( const char *this )
+void print_help( char *this )
 {
 	printf(
 		"\n"
 		"\n"
-		"	lognotify	%s	%s %s\n"
+		"LOGNOTIFY %s [%s %s]\n"
+		"\n"
+		"USAGE:\n"
+		"	%s <logfile1> <logfile2> ... <logfileN>\n"
 		"\n",
-		VERSION, __DATE__, __TIME__
-	);	
+		VERSION, __DATE__, __TIME__, this
+	);
 }
+
 int main( int argc, char *argv[] )
 {
-	print_help( (const char*) basename( argv[0] ) );
+	int i;
+
+	if( argc < 2 ) {
+		print_help( basename( argv[0] ) );
+		exit( EXIT_FAILURE );
+	}
+
+	/* dummy files count */
+	for( i = 0; i < argc - 1; i++ ) {
+		files[i] = argv[i+1];
+	}
 
 	daemon( 1, 1 );
 	daemon_main();
